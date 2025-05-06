2fe10b671835c843d89c1eeffa1379f55dc20785
chadda <chadha.essid@insat.ucar.tn>
2025-05-06 14:51:14 +0100

    fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,10 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "simple-rg"
   location = "East US"
@@ -22,16 +26,16 @@ resource "azurerm_linux_web_app" "webapp" {
   service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
-    linux_fx_version = "DOCKER|hello-world"
+    application_stack {
+      docker_image_name   = "hello-world"
+      docker_image_tag    = "latest"
+      docker_registry_url = "https://index.docker.io"
+    }
   }
 
   app_settings = {
     WEBSITES_PORT = "80"
   }
-}
-
-resource "random_id" "suffix" {
-  byte_length = 4
 }
 
 output "webapp_url" {
