9f3f3c6bfb13f6b840814595db29e67ff29d049a
chadda <chadha.essid@insat.ucar.tn>
2025-05-06 14:57:16 +0100

    fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,13 @@
 provider "azurerm" {
   features {}
+  resource_provider_registrations = "none"
 }
 
-resource "random_id" "suffix" {
-  byte_length = 4
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "simple-rg"
+resource "azurerm_resource_group" "test" {
+  name     = "jenkins-tf-rg"
   location = "East US"
 }
 
-resource "azurerm_service_plan" "plan" {
-  name                = "simple-plan"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  os_type             = "Linux"
-  sku_name            = "F1" # Free tier
-}
-
-resource "azurerm_linux_web_app" "webapp" {
-  name                = "simplewebapp-${random_id.suffix.hex}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan.plan.id
-
-  site_config {
-    application_stack {
-      docker_image_name   = "hello-world:latest"
-      docker_registry_url = "https://index.docker.io"
-    }
-  }
-
-
-  app_settings = {
-    WEBSITES_PORT = "80"
-  }
-}
-
-output "webapp_url" {
-  value = "https://${azurerm_linux_web_app.webapp.default_hostname}"
+output "resource_group_name" {
+  value = azurerm_resource_group.test.name
 }
