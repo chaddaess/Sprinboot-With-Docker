017c308d419a9fde2ab5e3f9197034342353ec49
chadda <chadha.essid@insat.ucar.tn>
2025-05-06 15:02:37 +0100

    fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 provider "azurerm" {
   features {}
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
   resource_provider_registrations = "none"
 }
 
-resource "azurerm_resource_group" "test" {
-  name     = "jenkins-tf-rg"
-  location = "East US"
-}
-
-output "resource_group_name" {
-  value = azurerm_resource_group.test.name
+resource "azurerm_resource_group" "devops_rg" {
+  name     = "devops-student-rg"
+  location = "westeurope"
 }
